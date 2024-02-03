e15df31bfc087a47568850d98356fd40663e21a8
widya Yasa <widyarusmananda15@gmail.com>
2024-02-04 02:24:57 +0800

    file starter setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Dashboard from "@/views/admin/Dashboard.vue";
+import LoginPage from "@/views/auth/LoginPage.vue";
+
+const testCondition = true
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect:'/dashboard',
+      // meta: {requireAuth: true}
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path:'/dashboard',
+      name:'dashboard',
+      component: Dashboard,
+      meta: {isPrivate: true}
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
+      meta: {isPrivate: false}
     }
   ]
 })
 
+router.beforeEach(async (to, from) => {
+  if (!testCondition && to.meta.isPrivate) {
+    return {name: "login"}
+  }
+  if (testCondition && !to.meta.isPrivate) {
+    return {name:"dashboard"}
+  }
+})
 export default router
