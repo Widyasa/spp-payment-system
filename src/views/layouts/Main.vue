53f678657e9f78ab17fd1de66546031c3b03fa53
widya Yasa <widyarusmananda15@gmail.com>
2024-02-19 00:02:08 +0800

    make main layout

---
+++ src/views/layouts/Main.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<template>
+  <div class="row">
+    <div class="col-2">
+      <Sidebar />
+    </div>
+    <div class="col-10">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
