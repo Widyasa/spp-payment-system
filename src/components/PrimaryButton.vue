c721718f0a851d34cd9a7de461bfad7b8548221c
widya Yasa <widyarusmananda15@gmail.com>
2024-02-19 00:02:17 +0800

    make main button component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps(['title','button_type'])
+</script>
+
+<template>
+  <button class="btn-primary btn" :type="button_type">{{title}}</button>
+</template>
+
+<style scoped>
+
+</style>
