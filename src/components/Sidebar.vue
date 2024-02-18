6269f5def09169b84a4065445512c6c59dea68d9
widya Yasa <widyarusmananda15@gmail.com>
2024-02-19 00:02:25 +0800

    make sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import {admin, petugas} from "@/helpers/global";
+</script>
+
+<template>
+  <div class="border-end h-100 min-vh-100 p-4">
+    <ul>
+      <li class="nav-link">
+        <RouterLink :to="{name: 'dashboard'}">Dashboard</RouterLink>
+      </li>
+      <li class="nav-link" v-show="admin || petugas">
+        <RouterLink :to="{name: 'siswa'}">Siswa</RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
