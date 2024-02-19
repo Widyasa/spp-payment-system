fa47ec5e47f3aac50e992c852a34d18c59d3a7c7
widya yasa <widyarusmananda15@gmail.com>
2024-02-19 13:54:38 +0800

    add the bearer on the main template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import axios from "axios";
+import {token} from "@/helpers/global";
+if (token.value){
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token.value}`
+}
 </script>
 
 <template>
