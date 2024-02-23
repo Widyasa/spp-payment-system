6035c337381af5e9b3f74b777088e8324e9a3d05
widya Yasa <widyarusmananda15@gmail.com>
2024-02-23 22:56:48 +0800

    make delete pembayaran modal and the action

---
+++ src/components/Pembayaran/DeletePembayaran.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+import {usePembayaranStore} from "@/stores/Pembayaran";
+
+const props = defineProps(['pembayaranId', 'message-pembayaran'])
+
+const {deletePembayaran} = usePembayaranStore()
+const emit = defineEmits(['list-pembayaran', 'message-pembayaran'])
+  const deletePembayaranModel = async () => {
+    try {
+      const response = await deletePembayaran(props.pembayaranId)
+      const {data, message} = response.data
+      if (response.status == 200) {
+        emit('list-pembayaran')
+        emit('message-pembayaran', response.data.message)
+      }
+    } catch (error) {
+      console.error(error?.message)
+    }
+  }
+</script>
+
+<template>
+  <div class="modal" id="modalPembayaranDelete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Hapus pembayaran</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form @submit.prevent="deletePembayaranModel">
+          Yakin Mau Hapus ?
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Hapus pembayaran</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
